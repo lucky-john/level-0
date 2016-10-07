e53368a16e57a74faa35ccc02840582bb8e00894
Savera <Level 2>
2016-10-06 17:58:46 -0700

    today we atempted  making a house

---
+++ Eclipse/Level-0/src/house.java
@@ -0,0 +1,30 @@
+
+public class house {
+	public static void main(String[] args) {
+		for (int i = 0; i < 4; i = i + 1) {
+		}
+
+	}
+
+	public static float getAngle(int sides) {
+		float total = sides * 180 - 360;
+		float angle = total / sides;
+		return angle;
+	}
+
+public static void drawshape(int sides)
+
+	
+{
+
+}
+new Robot = joey
+	joey.penDown();
+	float angle=getAngle(sides);
+			for (int i = 0; i < 4; i = i + 1) 
+			{
+				move(100);
+				turn(180-angle);
+			}
+	
+}
